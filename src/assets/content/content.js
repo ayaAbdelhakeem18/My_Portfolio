4c4cb104ae43c1cc38f86952adc2312aca97aca5
ayaAbdelhakeem18 <aya.abdelhakeem18@gmail.com>
2023-10-29 08:50:52 +0200

    changed intro text

--- src/assets/content/content.js
+++ src/assets/content/content.js
@@ -5,7 +5,7 @@ import img from "../images/photo.webp";
 const content={
     img1:img,
     img2:img,
-    about:"I'm a dynamic full-stack web developer with over two years of hands-on experience in crafting captivating frontend experiences and six months of dedicated backend development expertise. My passion for learning and growth is what drives me forward in this ever-evolving field. I specialize in utilizing React for the frontend and Laravel for the backend, striving to create seamless, feature-rich web applications. I'm particularly drawn to eCommerce projects, where I can leverage my skills to build exceptional online shopping experiences. I'm currently on the lookout for exciting eCommerce projects and opportunities to evolve my skills while collaborating effectively within a talented team. Let's build something amazing together!"    ,
+    about:"I excel in turning your web visions into reality through technical precision and meticulous attention to detail. With a strong background in web development, I'm dedicated to crafting websites that not only meet but exceed your expectations."    ,
     descriptiveTitle:'React and Laravel web Developer',
     projects:projects,
     services:services,
